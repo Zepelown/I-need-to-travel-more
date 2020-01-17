b78de5f6b0aaf30d0be571f83f8b4a2c69735d8a
KangMinGyo <49143316+gearhead64@users.noreply.github.com>
2020-01-17 18:29:09 +0900

    Firebase setting

---
+++ I-need-to-travel-more/README.md
@@ -0,0 +1,2 @@
+# I-need-to-travel-more
+여행 리뷰 어플
---
+++ app/src/main/java/com/example/i_need_to_travel_more/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.i_need_to_travel_more;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
